25b821b8269395d7cd4db2b5629072d3181e92af
akshatchoukse <choukseakshat@gmail.com>
2025-05-02 15:26:42 +0530

    year added

--- Models/StudentSchema.js
+++ Models/StudentSchema.js
@@ -3,7 +3,7 @@ const schema = new mongoose.Schema({
     college_name : String,
     student_name : String,
     student_contactno : Number , 
-    student_email : String,
+    student_year : Number,
     followup : String
 })
 module.exports = mongoose.model("StudentSchema" , schema)
